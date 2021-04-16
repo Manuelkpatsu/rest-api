b456456d8e2ae830b53cf0588342411ce05536a3
Manuelkpatsu <manuelkpatsu@gmail.com>
2021-04-16 14:26:12 +0000

    Add authorization middleware to all post routes

--- routes/feed.js
+++ routes/feed.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 router.post(
-    '/post', 
+    '/post',
+    isAuth,
     [
         body('title')
             .trim()
@@ -19,10 +21,11 @@ router.post(
     feedController.createPost
 );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 router.put(
     '/post/:postId',
+    isAuth,
     [
         body('title')
             .trim()
@@ -34,6 +37,6 @@ router.put(
     feedController.updatePost
 );
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
